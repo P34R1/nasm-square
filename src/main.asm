ec1e7421af959cb67710546d7c7642d672a1b407
pearl <undeadgamer279@gmail.com>
2024-11-07 21:32:22 -0500

    restructure code

--- src/main.asm
+++ src/main.asm
@@ -1,27 +1,16 @@
-  section .bss
-  arg resq 1
-
   section .text
   global _start
 
+  extern find_len
+
 _start:
-  mov rax, [rsp+16] ; argv[1]
-  mov [arg], rax
+  mov rdi, [rsp+16] ; argv[1]
+  call find_len
 
-; Calculate len of [arg]
-  xor rcx, rcx ; rcx = 0
-find_len:
-  mov rax, [arg]
-  cmp byte [rax + rcx], 0 ; check for \0
-  je print_arg
-  inc rcx ; rcx++
-  jmp find_len
-
-print_arg:
-  mov rsi, [arg]
+  mov rsi, rdi
+  mov rdx, rax ; str len
   mov rdi, 1
   mov rax, 1
-  mov rdx, rcx
   syscall
 
   mov rdi, 0
