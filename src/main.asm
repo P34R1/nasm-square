c960f8d2550b4d8d70502ec734f3688ead95ae1c
pearl <undeadgamer279@gmail.com>
2024-11-09 11:58:20 -0500

    add comments

--- src/main.asm
+++ src/main.asm
@@ -23,41 +23,50 @@ _start:
   mov rdi, [rsp+16]                    ; argv[1]
   call find_len
 
-; print str
+; print num
   mov rsi, rdi
   mov rdx, rax                         ; str len from find_len
   mov rdi, 1
   mov rax, 1
   syscall
 
+; print formatting "^2 = "
   mov rsi, math_fmt
   mov rdx, math_fmt_len                ; str len from find_len
   mov rdi, 1
   mov rax, 1
   syscall
 
+; convert to num
   mov rdi, [rsp+16]                    ; argv[1]
   call atoi
+
+; square
   mul rax                              ; square rax
 
+; after square convert to string
   mov rsi, rax
   call itoa                            ; Call the itoa function
 
+; print
   mov rsi, rax                         ; make rsi the beginning of the string
   mov rdx, rdx                         ; make rdx the string size
   mov rdi, 1
   mov rax, 1
   syscall
 
+; exit
   mov rdi, 0
   call exit
 
 argc_err:
+; print err
   mov rsi, argc_err_msg
   mov rdx, argc_err_len
   mov rdi, 1
   mov rax, 1
   syscall
 
+; exit with err code 1
   mov rdi, 1
   call exit
